497a7782789a48efcf8ddbe6a5c86dda0a3dc2d1
˹sᴜᴍɪᴛ ✗ ˼ <118965746+SUMIT0481@users.noreply.github.com>
2023-09-21 21:27:24 +0530

    Update start.py
    Signed-off-by: ˹sᴜᴍɪᴛ ✗ ˼ <118965746+SUMIT0481@users.noreply.github.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -36,9 +36,9 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            photo=random.choice(st)
-            return await message.reply_photo(
-                photo=photo,
+            animation=random.choice(st)
+            return await message.reply_animation(
+                animation=animation,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
