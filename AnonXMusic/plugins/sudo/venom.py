c2087f32f5379e263c8546058c26d57719ae16c0
˹sᴜᴍɪᴛ ✗ ˼ <118965746+SUMIT0481@users.noreply.github.com>
2023-12-27 19:39:31 +0530

    Create venom.py

---
+++ AnonXMusic/plugins/sudo/venom.py
@@ -0,0 +1,19 @@
+from config import LOGGER_ID 
+
+@app.on_message(filters.new_chat_members, group=2)
+async def join_watcher(_, message):    
+    chat = message.chat
+    for members in message.new_chat_members:
+        if members.id == app.id:
+            count = await app.get_chat_members_count(chat.id)
+
+            msg = (
+                f"📝 #New ᴍᴜsɪᴄ ʙᴏᴛ ᴀᴅᴅᴇᴅ ɪɴ ᴀ ɴᴇᴡ ɢʀᴏᴜᴘ\n\n"
+                f"\n\n"
+                f"📌 ᴄʜᴀᴛ ɴᴀᴍᴇ: {message.chat.title}\n"
+                f"🍂 ᴄʜᴀᴛ ɪᴅ: {message.chat.id}\n"
+                f"🔐 ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ: @{message.chat.username}\n"
+                f"📈 ɢʀᴏᴜᴘ ᴍᴇᴍʙᴇʀs: {count}\n"
+                f"🤔 ᴀᴅᴅᴇᴅ ʙʏ: {message.from_user.mention}"
+            )
+            await app.send_message(LOGGER_ID, text=msg)
